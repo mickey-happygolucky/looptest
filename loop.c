2843ac4d7fc7d738df42bcfc3e2ee6c95388c110
Yusuke Mitsuki <mickey.happygolucky@gmail.com>
2025-09-30 10:13:42 +0900

    Add a using static variable sample
    Signed-off-by: Yusuke Mitsuki <mickey.happygolucky@gmail.com>

--- loop.c
+++ loop.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
+static int st_num;
+
+
+int get_st_num(void) {
+        return st_num;
+}
+
 int loop_func(int num) {
         int i = 0;
         for (i = 0; i < num; i++) {
-                printf("i = %d\n", i);
+                printf("i = %d, st_num = %d\n", i, st_num);
                 if (i % 2) {
                         printf("%d is odd\n", i);
                 } else {
                         printf("%d is even\n", i);
                 }
+                st_num = i;
         }
         return num - i;
 }
--- main.c
+++ main.c
@@ -1,7 +1,16 @@
+#include <stdio.h>
+extern int get_st_num(void);
 extern int loop_func(int num);
 
 int main(void) {
         int var = 10;
-        loop_func(var);
+        int ret;
+
+        ret = get_st_num();
+        printf("get_st_num returned %d L:%d\n", ret, __LINE__);
+        ret = loop_func(var);
+        printf("loop_func returned %d\n", ret);
+        ret = get_st_num();
+        printf("get_st_num returned %d L:%d\n", ret, __LINE__);
         return 0;
 }
