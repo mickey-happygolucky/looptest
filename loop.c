0cd2171b078b612ecb814c5dd5f3fc0b518298a1
Yusuke Mitsuki <mickey.happygolucky@gmail.com>
2025-09-30 10:10:26 +0900

    Fix a small bug

--- loop.c
+++ loop.c
@@ -5,9 +5,9 @@ int loop_func(int num) {
         for (i = 0; i < num; i++) {
                 printf("i = %d\n", i);
                 if (i % 2) {
-                        printf("%d is odd\n");
+                        printf("%d is odd\n", i);
                 } else {
-                        printf("%d is even\n");
+                        printf("%d is even\n", i);
                 }
         }
         return num - i;
